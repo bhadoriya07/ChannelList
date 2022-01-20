9a599233a3a1104aa020fb257951ee73f74871a3
unknown <neeleshbhadoriya786@gmail.com>
2022-01-20 18:33:33 +0530

    urls updated

--- channelList/urls.py
+++ channelList/urls.py
@@ -3,9 +3,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from firstApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
     path('exportfile',views.exportfile,name="exportfile")
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
