72679cee553c1a9197bde702b55b13b9a350cfb3
unknown <neeleshbhadoriya786@gmail.com>
2022-01-20 18:29:10 +0530

    media file removed

--- channelList/urls.py
+++ channelList/urls.py
@@ -1,6 +1,5 @@
 import imp
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from firstApp import views
@@ -9,4 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
     path('exportfile',views.exportfile,name="exportfile")
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
