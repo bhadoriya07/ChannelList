5a4c63da7c9663851ba3666ea4101b2a6f7812f2
unknown <neeleshbhadoriya786@gmail.com>
2022-01-21 19:10:24 +0530

    index updated

--- firstApp/views.py
+++ firstApp/views.py
@@ -10,8 +10,8 @@ channel_list = []
 def home(request):
     context = {'message' : ""}
     if request.method == 'POST':
-        print("channel list at first: ")
-        print(channel_list)
+        # print("channel list at first: ")
+        # print(channel_list)
         rows = []
         video_id = []
         file = request.FILES["file"].readlines()
@@ -26,9 +26,9 @@ def home(request):
 
         video_id = [x.replace("\r\n","") for x in video_id]
 
-        print("Video id:")
-        for video in video_id:
-            print(video)
+        # print("Video id:")
+        # for video in video_id:
+        #     print(video)
 
         search_url = 'https://www.googleapis.com/youtube/v3/videos'
 
@@ -52,8 +52,8 @@ def home(request):
         
         [channel_list.append(x) for x in temp_list if x not in channel_list]
         
-        print("channel list at home:")
-        print(channel_list)
+        # print("channel list at home:")
+        # print(channel_list)
         
         context['message'] = "Click on Download File to download the file"
 
@@ -65,8 +65,8 @@ def exportfile(request):
 
     writer = csv.writer(response)
 
-    print("channel list at exportfile:")
-    print(channel_list)
+    # print("channel list at exportfile:")
+    # print(channel_list)
     
     for list in channel_list:
         writer.writerow([list['url']])
