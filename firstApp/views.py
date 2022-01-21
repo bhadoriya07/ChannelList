844d45a63762dc79427314fa21311c8230cb5354
unknown <neeleshbhadoriya786@gmail.com>
2022-01-21 20:46:43 +0530

    print added

--- firstApp/views.py
+++ firstApp/views.py
@@ -4,14 +4,15 @@ from django.shortcuts import render,HttpResponse
 from django.conf import settings
 import requests
 import csv
+from django.http import StreamingHttpResponse
 
 channel_list = []
 
 def home(request):
     context = {'message' : ""}
     if request.method == 'POST':
-        # print("channel list at first: ")
-        # print(channel_list)
+        print("channel list at first: ")
+        print(channel_list)
         rows = []
         video_id = []
         file = request.FILES["file"].readlines()
@@ -26,9 +27,9 @@ def home(request):
 
         video_id = [x.replace("\r\n","") for x in video_id]
 
-        # print("Video id:")
-        # for video in video_id:
-        #     print(video)
+        print("Video id:")
+        for video in video_id:
+            print(video)
 
         search_url = 'https://www.googleapis.com/youtube/v3/videos'
 
@@ -52,21 +53,23 @@ def home(request):
         
         [channel_list.append(x) for x in temp_list if x not in channel_list]
         
-        # print("channel list at home:")
-        # print(channel_list)
+        print("channel list at home:")
+        print(channel_list)
         
         context['message'] = "Click on Download File to download the file"
 
     return render(request,'index.html',context)
 
 def exportfile(request):
+
+
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachement; filename="channelUrls.csv"'
 
     writer = csv.writer(response)
 
-    # print("channel list at exportfile:")
-    # print(channel_list)
+    print("channel list at exportfile:")
+    print(channel_list)
     
     for list in channel_list:
         writer.writerow([list['url']])
